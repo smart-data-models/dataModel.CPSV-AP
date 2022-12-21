f16b94c6e14510a9d01787f083bb9338b39a41f5
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:18:16 +0100

    test of SQL export

---
+++ BusinessEvent/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model BusinessEvent of the subject dataModel.CPSV-AP for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE BusinessEvent_type AS ENUM ('BusinessEvent');
+CREATE TABLE BusinessEvent (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, eventType json, id text, identifier text, location json, name text, owner json, relatedService json, seeAlso json, source text, type BusinessEvent_type);
