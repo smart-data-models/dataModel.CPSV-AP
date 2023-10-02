a29f9b0b6eead694a17aa976bad10521964731f1
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:02:29 +0200

    test of SQL export

--- PublicOrganization/schema.sql
+++ PublicOrganization/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model PublicOrganization of the subject dataModel.CPSV-AP for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model PublicOrganization of the subject dataModel.CPSV-AP for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PublicOrganization_type AS ENUM ('PublicOrganization');
-CREATE TABLE PublicOrganization (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, preferredLabel text, seeAlso json, source text, spatial json, type PublicOrganization_type);
+CREATE TABLE PublicOrganization (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, preferredLabel TEXT, source TEXT, spatial JSON, type PublicOrganization_type);
