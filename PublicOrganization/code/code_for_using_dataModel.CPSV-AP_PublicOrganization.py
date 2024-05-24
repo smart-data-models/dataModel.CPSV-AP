ba28cf8917f8ac36f89411372ea5bcc2806919d8
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:42:10 +0200

    created example of code

--- PublicOrganization/code/code_for_using_dataModel.CPSV-AP_PublicOrganization.py
+++ PublicOrganization/code/code_for_using_dataModel.CPSV-AP_PublicOrganization.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "PublicOrganization"
 subject = "dataModel.CPSV-AP"
-preferredLabel = "{'type': 'string', 'value': 'The Finnish Patent and Registration Office'}"
+preferredLabel = "The Finnish Patent and Registration Office"
 attribute = "preferredLabel"
 value = preferredLabel
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-spatial = {'type': 'array', 'value': ['http://publications.europa.eu/resource/authority/atu/FIN']}
+spatial = ['http://publications.europa.eu/resource/authority/atu/FIN']
 attribute = "spatial"
 value = spatial
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
